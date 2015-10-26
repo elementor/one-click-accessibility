017aa3f15081bd9c7746c878f627700cce53bb73
Yakir Sitbon <kingyes1@gmail.com>
2015-10-26 19:02:50 +0200

    spell

--- pojo-accessibility.php
+++ pojo-accessibility.php
@@ -80,11 +80,11 @@ final class Pojo_Accessibility {
 	}
 
 	public function admin_notices() {
-		echo '<div class="error"><p>' . sprintf( __( '<a href="%s" target="_blank">Pojo Framework</a> is not active. Please activate any theme by Pojo before you are using "Pojo Accessibility" plugin.', 'pojo-accessibility' ), 'http://pojo.me/' ) . '</p></div>';
+		echo '<div class="error"><p>' . sprintf( __( '<a href="%s" target="_blank">Pojo Theme</a> is not active. Please activate any theme by Pojo.me before you are using "Pojo Accessibility" plugin.', 'pojo-accessibility' ), 'http://pojo.me/' ) . '</p></div>';
 	}
 
 	public function print_update_error() {
-		echo '<div class="error"><p>' . sprintf( __( 'Your <a href="%s" target="_blank">Pojo Framework</a> isn\'t updated, please upgrade.', 'pojo-accessibility' ), 'http://pojo.me/' ) . '</p></div>';
+		echo '<div class="error"><p>' . sprintf( __( 'The Pojo Accessibility is not supported by this version of %s. Please <a href="%s">upgrade the theme to its latest version</a>.', 'pojo-accessibility' ), Pojo_Core::instance()->licenses->updater->theme_name, admin_url( 'update-core.php' ) ) . '</p></div>';
 	}
 
 	public function bootstrap() {
