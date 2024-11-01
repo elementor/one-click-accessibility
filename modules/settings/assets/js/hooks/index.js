7938003b623f8a97f862ab96adee032872e687f3
Nirbhay <nirbhayr@elementor.com>
2024-11-01 13:18:45 +0530

    add: hooks

--- modules/settings/assets/js/hooks/index.js
+++ modules/settings/assets/js/hooks/index.js
@@ -1,2 +1,6 @@
 export { default as useAuth } from './use-auth';
 export { default as useModal } from './use-modal';
+export { useSettings } from './use-settings';
+export { useToastNotification } from './use-settings';
+export { default as SettingsProvider } from './use-settings';
+export { default as useStorage } from './use-storage';
