b5b0f662b0582ab2f086e72b47c13d1f7e982c8e
Nirbhay <nirbhayr@elementor.com>
2024-11-04 17:08:57 +0530

    add: icon export

---
+++ modules/settings/assets/js/icons/index.js
@@ -0,0 +1,5 @@
+export { default as ElementorLogo } from './elementor-logo';
+export { default as AccessibilityPersonIcon } from './accessibility-person';
+export { default as AccessibilityEyeIcon } from './accessibility-eye';
+export { default as AccessibilityTextIcon } from './accessibility-text';
+export { default as AccessibilityControlsIcon } from './accessibility-controls';
