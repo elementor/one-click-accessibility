2d63e55afe5be0ef5135d7f2267215ae1967f2b3
Nirbhay <nirbhayr@elementor.com>
2024-11-05 16:24:49 +0530

    update: color picker with react-colorful component

--- modules/settings/assets/js/components/color-picker/index.js
+++ modules/settings/assets/js/components/color-picker/index.js
@@ -1,12 +1,15 @@
+import Box from '@elementor/ui/Box';
 import FormControl from '@elementor/ui/FormControl';
 import FormLabel from '@elementor/ui/FormLabel';
+import Grid from '@elementor/ui/Grid';
 import Typography from '@elementor/ui/Typography';
-import { ColorPicker as WPColorPicker } from '@wordpress/components';
+import { HexColorPicker, HexColorInput } from 'react-colorful';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import './style.css';
 
 const ColorPicker = () => {
-	const [ color, setColor ] = useState();
+	const [ color, setColor ] = useState( '#2563eb' );
 
 	return (
 		<FormControl>
@@ -15,11 +18,32 @@ const ColorPicker = () => {
 					{ __( 'Color', 'pojo-accessibility' ) }
 				</Typography>
 			</FormLabel>
-			<WPColorPicker
-				color={ color }
-				onChange={ setColor }
-				defaultValue="#000"
-			/>
+			<Grid padding={ 1 }
+				border={ 1 }
+				borderColor="divider"
+				borderRadius={ 1 }
+			>
+				<HexColorPicker
+					color={ color }
+					onChange={ setColor }
+					defaultValue="#fff"
+					className="custom-color-picker"
+				/>
+				<Grid marginTop={ 2 } display="flex">
+					<Box padding={ 2 }
+						sx={ { backgroundColor: color } }
+						borderRadius={ 1 }
+						marginRight={ 1 }></Box>
+					<HexColorInput color={ color }
+						onChange={ setColor }
+						style={ {
+							width: '100%',
+							border: '1px solid rgba(0, 0, 0, 0.12)',
+							borderRadius: '3px',
+							paddingLeft: '10px',
+						} } />
+				</Grid>
+			</Grid>
 		</FormControl>
 	);
 };
