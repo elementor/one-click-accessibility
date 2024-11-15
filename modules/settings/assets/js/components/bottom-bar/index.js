e521973c4894eb431860e62fccd3f2807d6fe4a1
Nirbhay <nirbhayr@elementor.com>
2024-11-15 16:44:52 +0530

    fix: styling

--- modules/settings/assets/js/components/bottom-bar/index.js
+++ modules/settings/assets/js/components/bottom-bar/index.js
@@ -6,7 +6,8 @@ export const BottomBar = () => {
 		<Box display="flex"
 			justifyContent="end"
 			p={ 2 }
-			width="100%">
+			width="100%"
+			borderTop="1px solid rgba(0, 0, 0, 0.12)">
 			<Button variant="contained" color="info">Save Changes</Button>
 		</Box>
 	);
