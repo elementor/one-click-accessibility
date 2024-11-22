013fa74cec4ecb115fbe67b6552a50c8d87d64d3
Nirbhay <nirbhayr@elementor.com>
2024-11-22 17:40:36 +0530

    update: accessibility options rendering logic

--- modules/settings/assets/js/helpers/accessibility-options.js
+++ modules/settings/assets/js/helpers/accessibility-options.js
@@ -1,20 +1,22 @@
 import { __ } from '@wordpress/i18n';
 import { AccessibilityPersonIcon, AccessibilityTextIcon, AccessibilityEyeIcon, AccessibilityControlsIcon } from '../icons';
 
-const optionStyle = {
-	color: 'info.main',
-	fontSize: 44,
-};
+const options = () => {
+	const optionStyle = {
+		color: 'info.main',
+		fontSize: 44,
+	};
 
-const options = [
-	{ value: 'person', icon: <AccessibilityPersonIcon sx={ optionStyle } />, label: __( 'Accessibility Person Icon', 'pojo-accessibility' ) },
-	{ value: 'eye', icon: <AccessibilityEyeIcon sx={ optionStyle } />, label: __( 'Accessibility Eye Icon', 'pojo-accessibility' ) },
-	{ value: 'text', icon: <AccessibilityTextIcon sx={ optionStyle } />, label: __( 'Accessibility Text Badge Icon', 'pojo-accessibility' ) },
-	{ value: 'controls', icon: <AccessibilityControlsIcon sx={ optionStyle } />, label: __( 'Accessibility Controls Slider Icon', 'pojo-accessibility' ) },
-];
+	return [
+		{ value: 'person', icon: <AccessibilityPersonIcon sx={ optionStyle } />, label: __( 'Accessibility Person Icon', 'pojo-accessibility' ) },
+		{ value: 'eye', icon: <AccessibilityEyeIcon sx={ optionStyle } />, label: __( 'Accessibility Eye Icon', 'pojo-accessibility' ) },
+		{ value: 'text', icon: <AccessibilityTextIcon sx={ optionStyle } />, label: __( 'Accessibility Text Badge Icon', 'pojo-accessibility' ) },
+		{ value: 'controls', icon: <AccessibilityControlsIcon sx={ optionStyle } />, label: __( 'Accessibility Controls Slider Icon', 'pojo-accessibility' ) },
+	];
+};
 
 // Export the entire options array
 export default options;
 
 // Optionally, export a function to get a specific option by value
-export const getOptionByValue = ( value ) => options.find( ( option ) => option.value === value );
+export const getOptionByValue = ( value ) => options().find( ( option ) => option.value === value );
