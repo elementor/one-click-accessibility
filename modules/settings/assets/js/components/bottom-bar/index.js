d45ae6cb13fe38d054f54748532eeedb4d372e5f
Nirbhay <nirbhayr@elementor.com>
2024-12-02 14:38:11 +0530

    add: logics for handling and saving menu settings

--- modules/settings/assets/js/components/bottom-bar/index.js
+++ modules/settings/assets/js/components/bottom-bar/index.js
@@ -1,8 +1,28 @@
 import Box from '@elementor/ui/Box';
 import Button from '@elementor/ui/Button';
+import { useSettings, useStorage, useToastNotification } from '@ea11y/hooks';
 import { __ } from '@wordpress/i18n';
 
 export const BottomBar = () => {
+	const { selectedMenu, widgetMenuSettings, hasChanges, setHasChanges } =
+		useSettings();
+	const { save } = useStorage();
+	const { success, error } = useToastNotification();
+
+	const saveSettings = () => {
+		if (selectedMenu.parent === 'widget' && selectedMenu.child === 'menu') {
+			try {
+				save({
+					a11y_widget_menu_settings: widgetMenuSettings,
+				}).then(() => {
+					success('Settings saved!');
+					setHasChanges(false);
+				});
+			} catch (e) {
+				error('Failed to save settings!');
+			}
+		}
+	};
 	return (
 		<Box
 			display="flex"
@@ -11,7 +31,12 @@ export const BottomBar = () => {
 			width="100%"
 			borderTop="1px solid rgba(0, 0, 0, 0.12)"
 		>
-			<Button variant="contained" color="info">
+			<Button
+				variant="contained"
+				color="info"
+				onClick={saveSettings}
+				disabled={!hasChanges}
+			>
 				{__('Save Changes', 'pojo-accessibility')}
 			</Button>
 		</Box>
