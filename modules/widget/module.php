770ee525c8e5c9fdb12f76e3b2202f169089acca
Nirbhay <nirbhayr@elementor.com>
2024-12-26 17:09:21 +0530

    update: widget url, filter and enqueuing method

--- modules/widget/module.php
+++ modules/widget/module.php
@@ -7,7 +7,6 @@ use EA11y\Classes\Utils;
 use EA11y\Modules\Connect\Module as Connect;
 use EA11y\Classes\Logger;
 use EA11y\Modules\Settings\Classes\Settings;
-use EA11y\Classes\Services\Client;
 use Exception;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -21,62 +20,22 @@ class Module extends Module_Base {
 	}
 
 	/**
-	 * @return void
-	 * @throws Exception
-	 */
-	public function get_widget() : void {
-		if ( ! Connect::is_connected()  ) {
-			return;
-		}
-
-		$plan_data = Settings::get( Settings::PLAN_DATA );
-
-		if ( ! is_wp_error( $plan_data ) && ! empty( $plan_data ) && ! empty( $plan_data->public_api_key ) ) {
-
-			$widget_code = Utils::get_api_client()->make_request(
-				'GET',
-				'product/widget?api_key=' . $plan_data->public_api_key
-			);
-
-			if ( ! is_wp_error( $widget_code ) && ! empty( $widget_code ) ) {
-				wp_add_inline_script( 'ea11y-widget', $widget_code );
-			} else {
-				$this->show_widget_load_error();
-				Logger::error( esc_html( $widget_code->get_error_message() ) );
-			}
-
-		} else {
-			$this->show_widget_load_error();
-			Logger::error( esc_html( $plan_data->get_error_message() ) );
-		}
-	}
-
-	/**
-	 * Show a console error when widget fails to load
-	 * @return void
-	 */
-	private function show_widget_load_error () : void {
-		wp_add_inline_script('ea11y-widget', "console.error('Failed to load accessibility widget');");
-	}
-
-	/**
 	 * Enqueue scripts
 	 *
 	 * @return void
 	 * @throws Exception
 	 */
-	public function enqueue_global_assets () : void {
+	public function enqueue_accessibility_widget () : void {
+
+		$plan_data = Settings::get( Settings::PLAN_DATA );
 
 		wp_enqueue_script(
 			'ea11y-widget',
-			EA11Y_ASSETS_URL . '/js/widget.js',
+			$this->get_widget_url() .'?api_key=' . $plan_data->public_api_key,
 			[],
 			EA11Y_VERSION,
 			true
 		);
-
-		// fetch widget from service
-		$this->get_widget();
 
 		wp_localize_script(
 			'ea11y-widget',
@@ -88,10 +47,14 @@ class Module extends Module_Base {
 		);
 	}
 
+	private function get_widget_url() : string {
+		return apply_filters( 'ea11y_widget_url', '' ); // TODO: add public url
+	}
+
 	/**
 	 * Module constructor.
 	 */
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_global_assets' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_accessibility_widget' ] );
 	}
 }
