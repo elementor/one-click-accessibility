53be23ceafa4fb895552d370ad992bfa201c4740
Nirbhay <nirbhayr@elementor.com>
2024-12-30 10:36:28 +0530

    update: php compatibility with return types

--- modules/settings/classes/settings.php
+++ modules/settings/classes/settings.php
@@ -13,19 +13,24 @@ class Settings {
 	public const PLAN_DATA = 'ea11y_plan_data';
 
 	/**
-	 * Returns plugin settings data by option name typecasted to an appropriate data type.
+	 * Returns plugin settings data by option name
+	 * type cast to an appropriate data type.
 	 *
 	 * @param string $option_name
 	 * @return mixed
 	 */
-	public static function get( string $option_name ) :mixed {
-		$data = get_option( $option_name );
-
-		switch ( $option_name ) {
-			default:
-				return $data;
-		}
+	public static function get( string $option_name ) {
+		return get_option( $option_name );
 	}
+
+	/**
+	 * Update the settings data by option name.
+	 *
+	 * @param string $option_name
+	 * @param $value
+	 *
+	 * @return bool
+	 */
 	public static function set( string $option_name, $value ): bool {
 		return update_option( $option_name, $value, false );
 	}
