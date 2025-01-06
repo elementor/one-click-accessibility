a2fd500a62a6b9d370ee2dea5aa73feb5ac9bc22
Ohad <ohad@elementor.com>
2025-01-06 14:07:14 +0200

    ensure wp.ajax is loaded

--- modules/settings/components/settings-pointer.php
+++ modules/settings/components/settings-pointer.php
@@ -23,6 +23,7 @@ class Settings_Pointer {
 
 		wp_enqueue_script( 'wp-pointer' );
 		wp_enqueue_style( 'wp-pointer' );
+		wp_enqueue_script( 'wp-util' );
 
 		$pointer_content = '<h3>' . esc_html__( 'One Click Accessibility', 'pojo-accessibility' ) . '</h3>';
 		$pointer_content .= '<p>' . esc_html__( "Start setting up and customizing your site's accessibility widget.", 'pojo-accessibility' ) . '</p>';
