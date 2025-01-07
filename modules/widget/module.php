539b8d4b6db9b03f54e03ce2deba705136b182be
Nirbhay <nirbhayr@elementor.com>
2025-01-07 18:23:31 +0530

    update: enqueue widget for preview in the settings

--- modules/widget/module.php
+++ modules/widget/module.php
@@ -4,6 +4,7 @@ namespace EA11y\Modules\Widget;
 
 use EA11y\Classes\Module_Base;
 use EA11y\Modules\Connect\Module as Connect;
+use EA11y\Modules\Settings\Module as SettingsModule;
 use EA11y\Modules\Settings\Classes\Settings;
 use Exception;
 
@@ -48,7 +49,7 @@ class Module extends Module_Base {
 			'ea11yWidget',
 			[
 				'iconSettings' => get_option( 'ea11y_widget_icon_settings' ),
-				'menuSettings' => get_option( 'ea11y_widget_menu_settings' ),
+				'toolsSettings' => get_option( 'ea11y_widget_menu_settings' ),
 			]
 		);
 	}
@@ -58,9 +59,45 @@ class Module extends Module_Base {
 	}
 
 	/**
+	 * Load empty scripts in admin
+	 * @return void
+	 */
+	public function enqueue_accessibility_widget_admin () : void {
+		if ( ! Connect::is_connected() ) {
+			return;
+		}
+
+		$plan_data = json_decode(Settings::get( Settings::PLAN_DATA ));
+
+		if ( ! isset( $plan_data->public_api_key ) ) {
+			return;
+		}
+
+		wp_enqueue_script(
+			'ea11y-widget',
+			EA11Y_ASSETS_URL . '/js/widget.js',
+			[],
+			EA11Y_VERSION,
+			true
+		);
+
+		wp_localize_script(
+			'ea11y-widget',
+			'ea11yWidget',
+			[
+				'iconSettings' => get_option( 'ea11y_widget_icon_settings' ),
+				'toolsSettings' => get_option( 'ea11y_widget_menu_settings' ),
+				'preview' => true,
+				'previewContainer' => '#ea11y-widget-preview--container'
+			]
+		);
+	}
+
+	/**
 	 * Module constructor.
 	 */
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_accessibility_widget' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_accessibility_widget_admin' ] );
 	}
 }
