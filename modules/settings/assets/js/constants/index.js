b49c0f4659258688b96590388810d5151414ddc0
Nirbhay <nirbhayr@elementor.com>
2025-01-08 10:59:30 +0530

    update: store widget url in a constant

--- modules/settings/assets/js/constants/index.js
+++ modules/settings/assets/js/constants/index.js
@@ -1,3 +1,5 @@
 export const HELP_LINK = 'https://go.elementor.com/';
 export const UPGRADE_LINK = 'https://go.elementor.com/';
 export const BILLING_LINK = 'https://my.elementor.com/';
+export const WIDGET =
+	'http://localhost:10058/wp-content/apps-a11y-widget/dist/widget.js';
