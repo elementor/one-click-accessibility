3b82f789945f37a2278f593bc021e639e4633bb9
Nirbhay <nirbhayr@elementor.com>
2025-01-08 16:30:57 +0530

    update: add widget URL

--- modules/settings/assets/js/constants/index.js
+++ modules/settings/assets/js/constants/index.js
@@ -1,5 +1,4 @@
 export const HELP_LINK = 'https://go.elementor.com/';
 export const UPGRADE_LINK = 'https://go.elementor.com/';
 export const BILLING_LINK = 'https://my.elementor.com/';
-export const WIDGET =
-	'http://localhost:10058/wp-content/apps-a11y-widget/dist/widget.js';
+export const WIDGET = 'https://cdn.elementor.com/a11y/widget.js';
