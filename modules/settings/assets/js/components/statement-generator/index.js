66c5b81d4d2ba7afa1d20b4e70ea203b4e3e0b42
Nirbhay <nirbhayr@elementor.com>
2025-01-10 01:46:01 +0530

    update: restructure statement generator

--- modules/settings/assets/js/components/statement-generator/index.js
+++ modules/settings/assets/js/components/statement-generator/index.js
@@ -1,3 +1,4 @@
+import { FormHelperText } from '@elementor/ui';
 import Box from '@elementor/ui/Box';
 import Button from '@elementor/ui/Button';
 import Dialog from '@elementor/ui/Dialog';
@@ -6,6 +7,7 @@ import DialogContent from '@elementor/ui/DialogContent';
 import DialogHeader from '@elementor/ui/DialogHeader';
 import DialogTitle from '@elementor/ui/DialogTitle';
 import ErrorBoundary from '@elementor/ui/ErrorBoundary';
+import FormControl from '@elementor/ui/FormControl';
 import FormLabel from '@elementor/ui/FormLabel';
 import TextField from '@elementor/ui/TextField';
 import Typography from '@elementor/ui/Typography';
@@ -13,12 +15,20 @@ import { styled } from '@elementor/ui/styles';
 import { AlertError, HtmlToTypography } from '@ea11y/components';
 import { useSettings, useStorage } from '@ea11y/hooks';
 import apiFetch from '@wordpress/api-fetch';
+import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Statement } from '../../helpers/accessibility-statement';
+import {
+	parseContent,
+	checkEmail,
+	checkDomain,
+	checkCompanyName,
+} from '../../helpers/statement-generator';
 
 // Customization for the WP admin global CSS.
 const StyledTextField = styled(TextField)(() => ({
 	width: '100%',
+	marginTop: 8,
 	'.wp-admin & .MuiInputBase-input, & .MuiInputBase-input:focus': {
 		backgroundColor: 'initial',
 		boxShadow: 'none',
@@ -34,9 +44,42 @@ const StyledTextField = styled(TextField)(() => ({
 }));
 
 const StatementGenerator = ({ open, close }) => {
+	const [isValidName, setValidName] = useState(null);
+	const [isValidEmail, setValidEmail] = useState(null);
+	const [isValidDomain, setValidDomain] = useState(null);
+	const [disableCreateButton, setDisabledCreateButton] = useState(true);
+
 	const { companyData, setCompanyData, setAccessibilityStatementData } =
 		useSettings();
 	const { save } = useStorage();
+
+	useEffect(() => {
+		if (checkEmail(companyData?.company_email)) {
+			setValidEmail(true);
+		} else {
+			setValidEmail(false);
+		}
+
+		if (checkCompanyName(companyData?.company_name)) {
+			setValidName(true);
+		} else {
+			setValidName(false);
+		}
+
+		if (checkDomain(companyData?.company_website)) {
+			setValidDomain(true);
+		} else {
+			setValidDomain(false);
+		}
+	}, [companyData]);
+
+	useEffect(() => {
+		if (!isValidEmail || !isValidName || !isValidDomain) {
+			setDisabledCreateButton(true);
+		} else {
+			setDisabledCreateButton(false);
+		}
+	}, [isValidName, isValidEmail, isValidDomain]);
 
 	const handleClose = () => {
 		close();
@@ -49,20 +92,6 @@ const StatementGenerator = ({ open, close }) => {
 		}));
 	};
 
-	// Replace content of accessibility statement with user inputs.
-	const parseContent = (text, replacements) => {
-		let updatedText = text;
-
-		// Replace each placeholder with the corresponding value from replacements
-		Object.keys(replacements).forEach((key) => {
-			const placeholder = `{${key}}`; // Create placeholder format (e.g., {company_name})
-			const value = replacements[key]; // Get the replacement value
-			updatedText = updatedText.replace(new RegExp(placeholder, 'g'), value);
-		});
-
-		return updatedText;
-	};
-
 	const createPage = async () => {
 		await apiFetch({
 			method: 'POST',
@@ -73,7 +102,6 @@ const StatementGenerator = ({ open, close }) => {
 				status: 'publish',
 			},
 		}).then((response) => {
-			console.log(response);
 			setAccessibilityStatementData({
 				statement: parseContent(Statement, companyData),
 				pageId: response.id,
@@ -89,6 +117,10 @@ const StatementGenerator = ({ open, close }) => {
 				},
 			});
 
+			// Update accessibility statement URL in the global object.
+			if (window?.ea11yWidget) {
+				window.ea11yWidget.accessibilityStatementURL = response.link;
+			}
 			close();
 		});
 	};
@@ -108,7 +140,6 @@ const StatementGenerator = ({ open, close }) => {
 						{__('Statement generator', 'pojo-accessibility')}
 					</DialogTitle>
 				</DialogHeader>
-
 				<DialogContent dividers>
 					<Box display="grid" gridTemplateColumns="repeat(2, 1fr)" gap={8}>
 						<Box>
@@ -125,51 +156,72 @@ const StatementGenerator = ({ open, close }) => {
 									'pojo-accessibility',
 								)}
 							</Typography>
-							<FormLabel sx={{ marginBottom: 2 }}>
-								{__('Company name', 'pojo-accessibility')}
-							</FormLabel>
-							<StyledTextField
-								type="text"
-								variant="outlined"
-								size="small"
-								color="secondary"
-								sx={{ marginRight: 1 }}
-								margin="normal"
-								value={companyData.company_name}
-								onChange={(e) =>
-									updateCompanyData('company_name', e.currentTarget.value)
-								}
-							/>
-							<FormLabel sx={{ marginBottom: 2 }}>
-								{__('Company website', 'pojo-accessibility')}
-							</FormLabel>
-							<StyledTextField
-								type="text"
-								variant="outlined"
-								size="small"
-								color="secondary"
-								sx={{ marginRight: 1 }}
-								margin="normal"
-								value={companyData.company_website}
-								onChange={(e) =>
-									updateCompanyData('company_website', e.currentTarget.value)
-								}
-							/>
-							<FormLabel sx={{ marginBottom: 2 }}>
-								{__('Business email', 'pojo-accessibility')}
-							</FormLabel>
-							<StyledTextField
-								type="text"
-								variant="outlined"
-								size="small"
-								color="secondary"
-								sx={{ marginRight: 1 }}
-								margin="normal"
-								value={companyData.company_email}
-								onChange={(e) =>
-									updateCompanyData('company_email', e.currentTarget.value)
-								}
-							/>
+							<FormControl fullWidth>
+								<FormLabel>
+									{__('Company name', 'pojo-accessibility')}
+								</FormLabel>
+								<StyledTextField
+									type="text"
+									variant="outlined"
+									size="small"
+									color="secondary"
+									margin="normal"
+									value={companyData.company_name}
+									onChange={(e) =>
+										updateCompanyData('company_name', e.currentTarget.value)
+									}
+									error={isValidName === false}
+								/>
+								{!isValidName && (
+									<FormHelperText>
+										{__('Company name cannot be empty', 'pojo-accessibility')}
+									</FormHelperText>
+								)}
+							</FormControl>
+							<FormControl fullWidth>
+								<FormLabel>
+									{__('Company website', 'pojo-accessibility')}
+								</FormLabel>
+								<StyledTextField
+									type="text"
+									variant="outlined"
+									size="small"
+									color="secondary"
+									margin="normal"
+									value={companyData.company_website}
+									onChange={(e) =>
+										updateCompanyData('company_website', e.currentTarget.value)
+									}
+									error={isValidDomain === false}
+								/>
+								{!isValidDomain && (
+									<FormHelperText>
+										{__('Please enter a valid domain', 'pojo-accessibility')}
+									</FormHelperText>
+								)}
+							</FormControl>
+							<FormControl fullWidth>
+								<FormLabel>
+									{__('Business email', 'pojo-accessibility')}
+								</FormLabel>
+								<StyledTextField
+									type="text"
+									variant="outlined"
+									size="small"
+									color="secondary"
+									margin="normal"
+									value={companyData.company_email}
+									onChange={(e) =>
+										updateCompanyData('company_email', e.currentTarget.value)
+									}
+									error={isValidEmail === false}
+								/>
+								{!isValidEmail && (
+									<FormHelperText>
+										{__('Please enter a valid email', 'pojo-accessibility')}
+									</FormHelperText>
+								)}
+							</FormControl>
 						</Box>
 						<Box>
 							<Typography
@@ -201,7 +253,12 @@ const StatementGenerator = ({ open, close }) => {
 					<Button onClick={handleClose} color="secondary">
 						{__('Cancel', 'pojo-accessibility')}
 					</Button>
-					<Button onClick={createPage} variant="contained" color="info">
+					<Button
+						onClick={createPage}
+						variant="contained"
+						color="info"
+						disabled={disableCreateButton}
+					>
 						{__('Create statement & page', 'pojo-accessibility')}
 					</Button>
 				</DialogActions>
