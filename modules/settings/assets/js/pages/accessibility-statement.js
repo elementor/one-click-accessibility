ff94439c8d35690449a3b0dd989811b3c81803a3
Nirbhay <nirbhayr@elementor.com>
2025-01-09 13:24:33 +0530

    update: render statement page conditionally

--- modules/settings/assets/js/pages/accessibility-statement.js
+++ modules/settings/assets/js/pages/accessibility-statement.js
@@ -6,17 +6,19 @@ import Radio from '@elementor/ui/Radio';
 import RadioGroup from '@elementor/ui/RadioGroup';
 import Typography from '@elementor/ui/Typography';
 import { StatementGenerator } from '@ea11y/components';
-import { useModal } from '@ea11y/hooks';
+import { useModal, useSettings } from '@ea11y/hooks';
 import {
 	AccessibilityStatementExistingIcon,
 	AccessibilityStatementCreateIcon,
 } from '@ea11y/icons';
+import { StatementLink } from '@ea11y/layouts';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 const AccessibilityStatement = () => {
 	const { isOpen, open, close } = useModal(false);
 	const [statementOption, setStatementOption] = useState();
+	const { accessibilityStatementData } = useSettings();
 	return (
 		<>
 			<Container p={1} sx={{ overflow: 'auto', maxHeight: '100%', padding: 4 }}>
@@ -39,93 +41,106 @@ const AccessibilityStatement = () => {
 						'pojo-accessibility',
 					)}
 				</Typography>
-				<FormControl>
-					<FormLabel
-						id="icon-select-radio-buttons-group-label"
-						color="secondary"
-					>
-						<Typography variant="h6" marginBottom={1} color="text.primary">
-							{__('Need an accessibility statement?', 'pojo-accessibility')}
-						</Typography>
-						<Typography variant="body2" color="text.secondary" marginBottom={1}>
-							{__(
-								'You can have a statement created for you or use what you already have.',
-								'pojo-accessibility',
-							)}
-						</Typography>
-					</FormLabel>
-					<RadioGroup
-						aria-labelledby="icon-select-radio-buttons-group-label"
-						name="icon-select-radio-buttons-group"
-						value={statementOption}
-						sx={{
-							display: 'flex',
-							flexDirection: 'row',
-							flexWrap: 'nowrap',
-							gap: 2,
-						}}
-					>
-						<Paper
-							key="generate-accessibility-statement"
-							variant="outlined"
-							onClick={() => {
-								setStatementOption(false);
-								open();
-							}}
+				{!accessibilityStatementData?.pageId && (
+					<FormControl>
+						<FormLabel
+							id="icon-select-radio-buttons-group-label"
+							color="secondary"
+						>
+							<Typography variant="h6" marginBottom={1} color="text.primary">
+								{__('Need an accessibility statement?', 'pojo-accessibility')}
+							</Typography>
+							<Typography
+								variant="body2"
+								color="text.secondary"
+								marginBottom={1}
+							>
+								{__(
+									'You can have a statement created for you or use what you already have.',
+									'pojo-accessibility',
+								)}
+							</Typography>
+						</FormLabel>
+						<RadioGroup
+							aria-labelledby="icon-select-radio-buttons-group-label"
+							name="icon-select-radio-buttons-group"
+							value={statementOption}
 							sx={{
-								borderRadius: 'md',
-								boxShadow: 'sm',
 								display: 'flex',
-								flexDirection: 'column',
-								flexGrow: 1,
-								alignItems: 'center',
-								justifyContent: 'center',
-								gap: 1.5,
-								p: 2,
-								width: 329,
-								minHeight: 169,
-								borderColor:
-									statementOption === false ? 'info.main' : 'divider',
-								borderWidth: statementOption === false ? 2 : 1,
-								cursor: 'pointer',
+								flexDirection: 'row',
+								flexWrap: 'nowrap',
+								gap: 2,
 							}}
 						>
-							<AccessibilityStatementExistingIcon />
-							<Typography>
-								{__('Yes, I need one', 'pojo-accessibility')}
-							</Typography>
-							<Radio value={false} sx={{ opacity: 0, position: 'absolute' }} />
-						</Paper>
-						<Paper
-							key="existing-accessibility-statement"
-							variant="outlined"
-							onClick={() => setStatementOption(true)}
-							sx={{
-								borderRadius: 'md',
-								boxShadow: 'sm',
-								display: 'flex',
-								flexDirection: 'column',
-								flexGrow: 1,
-								alignItems: 'center',
-								justifyContent: 'center',
-								gap: 1.5,
-								p: 2,
-								minWidth: 10,
-								width: 329,
-								minHeight: 169,
-								borderColor: statementOption === true ? 'info.main' : 'divider',
-								borderWidth: statementOption === true ? 2 : 1,
-								cursor: 'pointer',
-							}}
-						>
-							<AccessibilityStatementCreateIcon />
-							<Typography>
-								{__('No, I already have one', 'pojo-accessibility')}
-							</Typography>
-							<Radio value={true} sx={{ opacity: 0, position: 'absolute' }} />
-						</Paper>
-					</RadioGroup>
-				</FormControl>
+							<Paper
+								key="generate-accessibility-statement"
+								variant="outlined"
+								onClick={() => {
+									setStatementOption(false);
+									open();
+								}}
+								sx={{
+									borderRadius: 'md',
+									boxShadow: 'sm',
+									display: 'flex',
+									flexDirection: 'column',
+									flexGrow: 1,
+									alignItems: 'center',
+									justifyContent: 'center',
+									gap: 1.5,
+									p: 2,
+									width: 329,
+									minHeight: 169,
+									borderColor:
+										statementOption === false ? 'info.main' : 'divider',
+									borderWidth: statementOption === false ? 2 : 1,
+									cursor: 'pointer',
+								}}
+							>
+								<AccessibilityStatementExistingIcon />
+								<Typography>
+									{__('Yes, I need one', 'pojo-accessibility')}
+								</Typography>
+								<Radio
+									value={false}
+									sx={{ opacity: 0, position: 'absolute' }}
+								/>
+							</Paper>
+							<Paper
+								key="existing-accessibility-statement"
+								variant="outlined"
+								onClick={() => setStatementOption(true)}
+								sx={{
+									borderRadius: 'md',
+									boxShadow: 'sm',
+									display: 'flex',
+									flexDirection: 'column',
+									flexGrow: 1,
+									alignItems: 'center',
+									justifyContent: 'center',
+									gap: 1.5,
+									p: 2,
+									minWidth: 10,
+									width: 329,
+									minHeight: 169,
+									borderColor:
+										statementOption === true ? 'info.main' : 'divider',
+									borderWidth: statementOption === true ? 2 : 1,
+									cursor: 'pointer',
+								}}
+							>
+								<AccessibilityStatementCreateIcon />
+								<Typography>
+									{__('No, I already have one', 'pojo-accessibility')}
+								</Typography>
+								<Radio value={true} sx={{ opacity: 0, position: 'absolute' }} />
+							</Paper>
+						</RadioGroup>
+					</FormControl>
+				)}
+				{(!statementOption || accessibilityStatementData?.pageId) && (
+					<StatementLink />
+				)}
 			</Container>
 			<StatementGenerator open={isOpen} close={close} />
 		</>
