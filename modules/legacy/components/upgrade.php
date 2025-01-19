4adf4aa6e2ef11596f4277a5f533afd9dab588d6
Ohad <ohad@elementor.com>
2025-01-19 17:27:04 +0200

    Tweaked Pointer strings Icon and CTA

--- modules/legacy/components/upgrade.php
+++ modules/legacy/components/upgrade.php
@@ -359,11 +359,11 @@ class Upgrade {
 		wp_enqueue_style( 'wp-pointer' );
 
 		$pointer_content = '<h3>' . esc_html__( 'Accessibility', 'pojo-accessibility' ) . '</h3>';
-		$pointer_content .= '<p>' . esc_html__( "Our new, improved accessibility widget is now available! Equally is packed with advanced styling options, improved feature controls, and so much more. ", 'pojo-accessibility' ) . '</p>';
+		$pointer_content .= '<p>' . esc_html__( "Our new, improved accessibility plugin is now available! Web Accessibility is packed with advanced styling options, improved feature controls, and so much more.", 'pojo-accessibility' ) . '</p>';
 
 		$pointer_content .= sprintf(
 			'<p><a class="button button-primary ea11y-pointer-settings-link" href="%s">%s</a></p>',
-			self::get_switch_now_link(),
+			admin_url( 'admin.php?page=accessibility-settings' ),
 			esc_html__( 'View more details', 'pojo-accessibility' )
 		);
 
@@ -413,7 +413,8 @@ class Upgrade {
 			/* TODO: Update icon */
 			.ea11y-settings-pointer .wp-pointer-content h3::before {
 				content: '';
-				background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' fill='none'%3E%3Cg clip-path='url(%23a)'%3E%3Ccircle cx='16' cy='16' r='16' fill='%23FF7BE5'/%3E%3Cpath fill='%23fff' fill-rule='evenodd' d='M15.945 7.707a.142.142 0 0 1 .11 0l9.981 4.212c.3.126.496.42.496.746V23.9a.405.405 0 0 1-.405.404H5.872a.405.405 0 0 1-.405-.404V12.168L16 15.798l3.443-.507L16 17.013v7.183a.04.04 0 0 0 .07.027l10.122-11.567a.304.304 0 0 0-.229-.504H5.468v-.024l10.477-4.42Z' clip-rule='evenodd'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath fill='%23fff' d='M0 0h32v32H0z'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E%0A");
+				background-size: contain;
+				background-image: url("<?php echo esc_url( EA11Y_ASSETS_URL . 'images/logo.svg' ); ?>");
 			}
 		</style>
 		<?php
