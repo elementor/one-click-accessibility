5e13c0d59cfb2c729bb85ce23c65378e98ca9a7f
VasylD <vasyld@elementor.red>
2025-01-21 06:47:27 +0200

    [APP-991] Add translation for statement (#142)
    * [APP-979] Update links and plugin name
    
    * [APP-991] Add translation for statement

--- modules/settings/assets/js/helpers/accessibility-statement.js
+++ modules/settings/assets/js/helpers/accessibility-statement.js
@@ -1,34 +1,37 @@
+import { __, sprintf } from '@wordpress/i18n';
+
+/* eslint-disable @wordpress/i18n-translator-comments */
 export const Statement = `
 <!-- wp:heading {"level":6} -->
-<h6>Accessibility Statement for {company_website}</h6>
+<h6>${sprintf(__('Accessibility Statement for %s', 'pojo-accessibility'), '{company_website}')}</h6>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
-<p>{company_name} is committed to ensuring digital accessibility for people with disabilities. We are continually improving the user experience for everyone and applying the relevant accessibility standards.</p>
+<p>${sprintf(__('%s is committed to ensuring digital accessibility for people with disabilities. We are continually improving the user experience for everyone and applying the relevant accessibility standards.', 'pojo-accessibility'), '{company_name}')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:heading {"level":6} -->
-<h6>Conformance status</h6>
+<h6>${__('Conformance status', 'pojo-accessibility')}</h6>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
-<p>The Web Content Accessibility Guidelines (WCAG) defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA.</p>
+<p>${__('The Web Content Accessibility Guidelines (WCAG) defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA.', 'pojo-accessibility')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p>As of the date of this statement, {company_name} website is partially conformant with WCAG 2.2 level AA. Partially conformant means that some parts of the content do not fully conform to the accessibility standard.</p>
+<p>${sprintf(__('As of the date of this statement, %s website is partially conformant with WCAG 2.2 level AA. Partially conformant means that some parts of the content do not fully conform to the accessibility standard.', 'pojo-accessibility'), '{company_name}')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p>We are currently working on remediating our website to ensure the best quality usability for all of our users.</p>
+<p>${__('We are currently working on remediating our website to ensure the best quality usability for all of our users.', 'pojo-accessibility')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:heading {"level":6} -->
-<h6>Feedback</h6>
+<h6>${__('Feedback', 'pojo-accessibility')}</h6>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
-<p>We welcome your feedback on the accessibility of {company_name} website. Please let us know if you encounter accessibility barriers on our website:</p>
+<p>${sprintf(__('We welcome your feedback on the accessibility of %s website. Please let us know if you encounter accessibility barriers on our website:', 'pojo-accessibility'), '{company_name}')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p>E-mail: {company_email}</p>
+<p>${sprintf(__('E-mail: %s', 'pojo-accessibility'), '{company_email}')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p>We try to respond to feedback within 3-5 business days.</p>
+<p>${__('We try to respond to feedback within 3–5 business days.', 'pojo-accessibility')}</p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p>This statement was created on {current_date}. </p>
+<p>${sprintf(__('This statement was created on %s.', 'pojo-accessibility'), '{current_date}')} </p>
 <!-- /wp:paragraph -->`;
