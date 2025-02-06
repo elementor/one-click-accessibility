6c6fef7a7983e4a722bf4c6cebc9da1316674a44
Nirbhay <nirbhayr@elementor.com>
2025-02-06 11:05:10 +0530

    update: add admin_url and generate query args properly

--- modules/settings/assets/js/components/edit-link/index.js
+++ modules/settings/assets/js/components/edit-link/index.js
@@ -3,20 +3,24 @@ import IconButton from '@elementor/ui/IconButton';
 import Tooltip from '@elementor/ui/Tooltip';
 import { useSettings } from '@ea11y/hooks';
 import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 
 const EditLink = () => {
 	const { accessibilityStatementData } = useSettings();
 	const handleEdit = () => {
-		window.open(
-			`/wp-admin/post.php?post=${accessibilityStatementData?.pageId}&action=edit`,
-			'_blank',
-		);
+		const adminUrl = window?.ea11ySettingsData?.adminUrl;
+		const url = addQueryArgs(`${adminUrl}post.php`, {
+			post: accessibilityStatementData?.pageId,
+			action: 'edit',
+		});
+		window.open(url, '_blank');
 	};
+
 	return (
 		<Tooltip
 			placement="top"
 			title={__('Edit page', 'pojo-accessibility')}
-			arrow={true}
+			arrow
 			PopperProps={{
 				modifiers: [
 					{
@@ -36,7 +40,11 @@ const EditLink = () => {
 				],
 			}}
 		>
-			<IconButton onClick={handleEdit} sx={{ marginLeft: 1 }}>
+			<IconButton
+				onClick={handleEdit}
+				sx={{ marginLeft: 1 }}
+				aria-label="Edit accessibility statement page"
+			>
 				<EditIcon />
 			</IconButton>
 		</Tooltip>
