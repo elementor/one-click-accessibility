e6386b5367a2ce43dc55c70286d6d94b45e6974b
Nirbhay Singh <nirbhayr@elementor.com>
2025-03-17 13:45:06 +0530

    add: GOLINKS

--- modules/settings/assets/js/constants/index.js
+++ modules/settings/assets/js/constants/index.js
@@ -15,3 +15,18 @@ export const WIDGET_PREVIEW_ROOT = 'ea11y-root';
 export const PRO_FEATURES = {
 	REMOVE_BRANDING: 'remove-elementor-label',
 };
+
+export const GOLINKS = {
+	UPGRADE_80: 'https://go.elementor.com/acc-80-quota',
+	UPGRADE_95: 'https://go.elementor.com/acc-95-quota',
+	UPGRADE_100: 'https://go.elementor.com/acc-100-quota',
+	SCREEN_READER_ICON: 'https://go.elementor.com/acc-screen-reader-icon',
+	SCREEN_READER_TOGGLE: 'https://go.elementor.com/acc-screen-reader-toggle',
+	ALLY_LABEL_ICON: 'https://go.elementor.com/acc-label-icon',
+	ALLY_LABEL_TOGGLE: 'https://go.elementor.com/acc-label-toggle',
+	ANALYTICS_POPUP: 'https://go.elementor.com/acc-stats-popup',
+	ADD_VISITS: 'https://go.elementor.com/acc-add-visits',
+	HELP: 'https://go.elementor.com/acc-main-help',
+	LEARN_MORE_STATEMENT: 'https://go.elementor.com/acc-statement-learn-more',
+	UPGRADE: 'https://go.elementor.com/acc-upgrade',
+};
