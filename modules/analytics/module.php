f397efa8a91c3beae0856815609dae536c4fcd77
vasyldinets <vasyld@elementor.red>
2025-03-21 14:16:01 +0100

    [APP-1262] Enable routes for get statistic on disabled analytics

--- modules/analytics/module.php
+++ modules/analytics/module.php
@@ -4,6 +4,7 @@ namespace EA11y\Modules\Analytics;
 
 use EA11y\Classes\Module_Base;
 use EA11y\Modules\Analytics\Database\Analytics_Table;
+use EA11y\Modules\Analytics\Rest\Statistic;
 use EA11y\Modules\Settings\Classes\Settings;
 use EA11y\Modules\Settings\Module as SettingsModule;
 
@@ -36,15 +37,22 @@ class Module extends Module_Base {
 	public static function routes_list() : array {
 		return [
 			'Events',
-			'Statistic',
 		];
 	}
 
+	public static function register_required_routes() {
+		if ( is_admin() ) {
+			new Statistic();
+		}
+	}
+
 	public static function is_active(): bool {
+		self::register_required_routes();
 		$plan_data = Settings::get( Settings::PLAN_DATA );
 		return (
 			isset( $plan_data->plan->features->analytics ) &&
-			$plan_data->plan->features->analytics
+			$plan_data->plan->features->analytics &&
+			Settings::get( Settings::ANALYTICS_SETTINGS )
 		);
 	}
 
--- modules/analytics/rest/events.php
+++ modules/analytics/rest/events.php
@@ -32,26 +32,26 @@ class Events extends Route_Base {
 	 * @return WP_REST_Response
 	 */
 	public function POST( WP_REST_Request $request ): WP_REST_Response {
-		if ( Settings::get( Settings::ANALYTICS_SETTINGS ) ) {
-			try {
-				$raw_data = $request->get_body();
-				$events = json_decode( $raw_data, true );
-				foreach ( $events as $item ) {
-					if ( Analytics_Entry::validate_item( $item['event'] ) ) {
-						$value = array_key_exists( 'value', $item ) ? sanitize_text_field( $item['value'] ) : null;
-						$analytics_entry = new Analytics_Entry([
-							'data' => [
-								Analytics_Table::EVENT => $item['event'],
-								Analytics_Table::VALUE => $value,
-							],
-						]);
-						$analytics_entry->create();
-					}
+
+		try {
+			$raw_data = $request->get_body();
+			$events = json_decode( $raw_data, true );
+			foreach ( $events as $item ) {
+				if ( Analytics_Entry::validate_item( $item['event'] ) ) {
+					$value = array_key_exists( 'value', $item ) ? sanitize_text_field( $item['value'] ) : null;
+					$analytics_entry = new Analytics_Entry([
+						'data' => [
+							Analytics_Table::EVENT => $item['event'],
+							Analytics_Table::VALUE => $value,
+						],
+					]);
+					$analytics_entry->create();
 				}
-			} catch ( Throwable $t ) {
-				Logger::info( $t->getMessage() );
 			}
+		} catch ( Throwable $t ) {
+			Logger::info( $t->getMessage() );
 		}
+
 		return new WP_REST_Response( null, 204 );
 	}
 }
