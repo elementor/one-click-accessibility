05288d0ccdf561d92c6c5f9ae4aaaccc7d4af48a
vasyldinets <vasyld@elementor.red>
2025-05-02 23:35:49 +0200

    Add texts, add remediation submit, fix logic to store remediation

--- modules/remediation/module.php
+++ modules/remediation/module.php
@@ -38,7 +38,5 @@ class Module extends Module_Base {
 		Page_Table::install();
 		$this->register_routes();
 		$this->register_components();
-		add_action( 'template_redirect', [ $this, 'start' ] );
-
 	}
 }
