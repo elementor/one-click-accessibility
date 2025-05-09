dcde636ae23e396e62c115813d2bebca2ec9a722
Nirbhay Singh <nirbhayr@elementor.com>
2025-05-09 17:06:12 +0530

    fix: styling of custom svg

--- modules/settings/assets/js/components/custom-icon/index.js
+++ modules/settings/assets/js/components/custom-icon/index.js
@@ -1,31 +1,35 @@
+import { styled } from '@elementor/ui/styles';
 import { useSettings } from '@ea11y/hooks';
-import { useEffect, useState } from '@wordpress/element';
 
 const CustomIcon = ({ size }) => {
 	const { iconDesign } = useSettings();
-	const [svg, setSvg] = useState(iconDesign.custom.svg);
 	const strokeColor = `lch(from ${iconDesign?.color || '#fff'} calc((50 - l) * 100) 0 0)`;
 
-	useEffect(() => {
-		// add style to the svg code
-		const svgCode = iconDesign.custom.svg.replace(
-			/<svg /,
-			`<svg style="font-size:${size}px;width: ${size}px;fill:${strokeColor}" `,
-		);
-		setSvg(svgCode);
-	}, [iconDesign]);
-
 	return (
-		<div
+		<StyledDiv
 			className="custom-icon"
 			style={{
 				display: 'flex',
 				alignItems: 'center',
 				justifyContent: 'center',
 			}}
-			dangerouslySetInnerHTML={{ __html: svg }}
+			dangerouslySetInnerHTML={{ __html: iconDesign?.custom?.svg }}
+			size={size}
+			strokeColor={strokeColor}
 		/>
 	);
 };
 
 export default CustomIcon;
+
+const StyledDiv = styled('div')`
+	svg {
+		font-size: ${({ size }) => size}px;
+		width: ${({ size }) => size}px;
+		fill: ${({ strokeColor }) => strokeColor};
+
+		path {
+			fill: ${({ strokeColor }) => strokeColor};
+		}
+	}
+`;
