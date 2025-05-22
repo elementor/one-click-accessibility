e29caa9032a07454bfb37a881a384bbe0261af9b
Nirbhay Singh <nirbhayr@elementor.com>
2025-05-22 17:19:37 +0530

    update: AI credits name

--- modules/settings/assets/js/components/quota-bar/data.js
+++ modules/settings/assets/js/components/quota-bar/data.js
@@ -16,7 +16,7 @@ export const QuotaBarData = {
 		),
 	},
 	ai: {
-		title: __('AI credits used', 'pojo-accessibility'),
+		title: __('AI credits', 'pojo-accessibility'),
 		infotipDescription: __(
 			'This shows how many times your accessibility widget has loaded for unique visitors across all your connected sites this monthly cycle (each IP/device is counted once per 24 hours). If you’re nearing your plan’s monthly limit, you can upgrade to keep all features available.',
 			'pojo-accessibility',
