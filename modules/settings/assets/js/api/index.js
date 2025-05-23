7a09f81ef9ec9edaa841d5cf24b71f4ae7f26dfb
Nirbhay Singh <nirbhayr@elementor.com>
2025-05-23 17:02:05 +0530

    update: refactor api base function to globals

--- modules/settings/assets/js/api/index.js
+++ modules/settings/assets/js/api/index.js
@@ -1,41 +1,10 @@
-import apiFetch from '@wordpress/api-fetch';
+import { APIBase } from '@ea11y/globals';
 import { addQueryArgs } from '@wordpress/url';
-import APIError from './exceptions/APIError';
 
 const wpV2Prefix = '/wp/v2';
 const v1Prefix = '/ea11y/v1';
 
-class API {
-	static async request({ path, data, method = 'POST' }) {
-		try {
-			if ('GET' === method && !path.startsWith(wpV2Prefix)) {
-				path = addQueryArgs(path, { sb_time: new Date().getTime() });
-			}
-
-			const response = await apiFetch({
-				path,
-				method,
-				data,
-			});
-
-			if (path.startsWith(wpV2Prefix)) {
-				return response;
-			}
-
-			if (!response.success) {
-				throw new APIError(response.data.message);
-			}
-
-			return response.data;
-		} catch (e) {
-			if (e instanceof APIError) {
-				throw e;
-			} else {
-				throw new APIError(e.message);
-			}
-		}
-	}
-
+class API extends APIBase {
 	static async initConnect(context = 'new') {
 		const data = {
 			wp_rest: window?.ea11ySettingsData?.wpRestNonce,
