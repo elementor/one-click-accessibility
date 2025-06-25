2908c3b212a8ca18869a931a4aebb239b0ca6743
Nirbhay Singh <nirbhayr@elementor.com>
2025-06-25 19:46:49 +0530

    fix: width issues

--- modules/settings/assets/js/pages/icon-settings.js
+++ modules/settings/assets/js/pages/icon-settings.js
@@ -1,10 +1,8 @@
+import Typography from '@elementor/ui/Typography';
+import { styled } from '@elementor/ui/styles';
 import { BottomBar } from '@ea11y/components';
 import { IconDesignSettings, PositionSettings } from '@ea11y/layouts';
-import {
-	StyledBox,
-	StyledTitle,
-	StyledWideBox,
-} from '@ea11y/pages/pages.styles';
+import { StyledBox, StyledWideBox } from '@ea11y/pages/pages.styles';
 import { mixpanelEvents, mixpanelService } from '@ea11y-apps/global/services';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -33,3 +31,14 @@ const IconSettings = () => {
 };
 
 export default IconSettings;
+
+const StyledTitle = styled(Typography)`
+	max-width: 1200px;
+	// Spacing
+	margin-bottom: 16px;
+	margin-right: auto;
+	margin-left: auto;
+	// Typography
+	font-weight: 400;
+	letter-spacing: 0.25px;
+`;
--- modules/settings/assets/js/pages/menu.js
+++ modules/settings/assets/js/pages/menu.js
@@ -1,13 +1,10 @@
 import Box from '@elementor/ui/Box';
+import Typography from '@elementor/ui/Typography';
 import { styled } from '@elementor/ui/styles';
 import { BottomBar } from '@ea11y/components';
 import SkipToContentSettings from '@ea11y/components/skip-to-content-settings';
 import { MenuSettings, WidgetPreview } from '@ea11y/layouts';
-import {
-	StyledBox,
-	StyledTitle,
-	StyledWideBox,
-} from '@ea11y/pages/pages.styles';
+import { StyledBox, StyledWideBox } from '@ea11y/pages/pages.styles';
 import { mixpanelEvents, mixpanelService } from '@ea11y-apps/global/services';
 import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
@@ -22,11 +19,7 @@ const Menu = () => {
 	return (
 		<StyledBox>
 			<StyledWideBox>
-				<StyledTitle
-					variant="h4"
-					color="text.primary"
-					sx={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}
-				>
+				<StyledTitle variant="h4" color="text.primary">
 					{__('Capabilities', 'pojo-accessibility')}
 				</StyledTitle>
 
@@ -48,11 +41,16 @@ const StyledSettingsWrapper = styled(Box)`
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	gap: ${({ theme }) => theme.spacing(4)};
-	width: 50%;
+	max-width: 1200px;
 	margin-left: auto;
 	margin-right: auto;
+`;
 
-	${({ theme }) => theme.breakpoints.down('xl')} {
-		width: 100%;
-	}
+const StyledTitle = styled(Typography)`
+	font-weight: 400;
+	letter-spacing: 0.25px;
+	margin-bottom: 16px;
+	max-width: 1200px;
+	margin-right: auto;
+	margin-left: auto;
 `;
