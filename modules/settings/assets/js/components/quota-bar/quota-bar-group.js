203e588e44002b596d9ea0d9d81bd39ffd118a5d
Nirbhay Singh <121793120+nirbhayel@users.noreply.github.com>
2025-07-04 16:22:08 +0530

    Update modules/settings/assets/js/components/quota-bar/quota-bar-group.js
    Co-authored-by: gitstream-cm[bot] <111687743+gitstream-cm[bot]@users.noreply.github.com>

--- modules/settings/assets/js/components/quota-bar/quota-bar-group.js
+++ modules/settings/assets/js/components/quota-bar/quota-bar-group.js
@@ -27,7 +27,6 @@ const QuotaBarGroup = ({ collapsible = true, popup = false }) => {
 
 	const toggleOpen = () => setOpen((prev) => !prev);
 
-	console.log(planData);
 
 	/**
 	 * Send an event to the Mixpanel when the user clicks on the "Add visits" button and open the link.
