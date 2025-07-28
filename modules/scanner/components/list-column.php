a1535fc460acbf2573620a57b08ddd1f1babdcf5
Nirbhay Singh <nirbhayr@elementor.com>
2025-07-28 11:35:17 +0530

    fix: phpcs issues

--- modules/scanner/components/list-column.php
+++ modules/scanner/components/list-column.php
@@ -66,14 +66,14 @@ class List_Column {
 		$has_scan_data = $page->exists();
 		$latest_scan = Scan_Entry::get_scan_result( $url_trimmed, true );
 
-        if ( empty( $latest_scan ) ) {
-            $latest_scan = [
-                'counts' => [
-                    'violation' => 0,
-                    'issuesResolved' => 0,
-                ],
-            ];
-        }
+		if ( empty( $latest_scan ) ) {
+			$latest_scan = [
+				'counts' => [
+					'violation' => 0,
+					'issuesResolved' => 0,
+				],
+			];
+		}
 
 		$violation = $latest_scan['counts']['violation'];
 		$resolved = $latest_scan['counts']['issuesResolved'];
