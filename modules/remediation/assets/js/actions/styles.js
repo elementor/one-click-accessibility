1ffc9210bc802809ee46cae8de8844616fe28685
vasyldinets <vasyld@elementor.red>
2025-08-04 16:28:42 +0200

    Exclude adminbar from css rule

--- modules/remediation/assets/js/actions/styles.js
+++ modules/remediation/assets/js/actions/styles.js
@@ -22,6 +22,19 @@ export class StylesRemediation extends RemediationBase {
 		return this.dom.querySelector('style#ea11y-remediation-styles');
 	}
 
+	excludeAdminBar() {
+		const adminBar = this.dom.querySelector('div#wpadminbar');
+		if (!adminBar) {
+			return this.data.rule;
+		}
+		const replacement = window.ea11yScannerData ? 'div' : 'div:nth-of-type(2)';
+
+		return this.data.rule.replaceAll(
+			'html:first-of-type > body:first-of-type > div:first-of-type',
+			`html:first-of-type > body:first-of-type > ${replacement}`,
+		);
+	}
+
 	run() {
 		const tag = this.getStyleTag();
 
@@ -29,7 +42,8 @@ export class StylesRemediation extends RemediationBase {
 			return false;
 		}
 
-		tag.innerText += this.data.rule;
+		const rule = this.excludeAdminBar();
+		tag.innerText += rule;
 
 		return true;
 	}
