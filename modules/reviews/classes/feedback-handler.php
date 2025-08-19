b60acf3ac650636cbf90156e6ad63c23a999fc0a
Nirbhay Singh <nirbhayr@elementor.com>
2025-08-19 18:17:35 +0530

    fix: endpoint for feedback

--- modules/reviews/classes/feedback-handler.php
+++ modules/reviews/classes/feedback-handler.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Feedback_Handler {
 
-	const SERVICE_ENDPOINT = 'reviews';
+	const SERVICE_ENDPOINT = 'feedback/reviews';
 
 	/**
 	 * Send request to the service to submit the feedback.
