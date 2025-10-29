c6f2aea63360be8f0ec53572f79a169eb5529f63
vasyldinets <vasyld@elementor.red>
2025-10-29 13:08:56 +0100

    [APP-2003] add disable across scans

--- modules/scanner/assets/js/layouts/management/manage-main-layout.js
+++ modules/scanner/assets/js/layouts/management/manage-main-layout.js
@@ -1,18 +1,43 @@
+import Alert from '@elementor/ui/Alert';
 import Box from '@elementor/ui/Box';
+import Button from '@elementor/ui/Button';
 import CardContent from '@elementor/ui/CardContent';
 import Typography from '@elementor/ui/Typography';
 import { styled } from '@elementor/ui/styles';
+import CrownFilled from '@ea11y-apps/global/icons/crown-filled';
 import { EmptyManageMessage } from '@ea11y-apps/scanner/components/empty-manage-message';
 import ManageRemediationList from '@ea11y-apps/scanner/components/manage-remediation-list';
 import {
 	ManageGlobalRemediationControl,
 	ManageRemediationControl,
 } from '@ea11y-apps/scanner/components/manage-remediation-main-controls';
+import {
+	HIDE_UPGRADE_KEY,
+	IS_PRO_PLAN,
+	ONE_WEEK_IN_MS,
+	UPGRADE_URL,
+} from '@ea11y-apps/scanner/constants';
 import { useScannerWizardContext } from '@ea11y-apps/scanner/context/scanner-wizard-context';
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 export const ManageMainLayout = () => {
 	const { remediations, globalRemediations } = useScannerWizardContext();
+	const [showUpgradeAlert, setShowUpgradeAlert] = useState(true);
+
+	const hideUpgradeAlert = () => {
+		window.localStorage.setItem(HIDE_UPGRADE_KEY, Date.now().toString());
+		setShowUpgradeAlert(false);
+	};
+
+	const isShowUpgradeAlert = () => {
+		const time = window.localStorage.getItem(HIDE_UPGRADE_KEY);
+		if (!time || showUpgradeAlert) {
+			return true;
+		}
+		const diff = Date.now() - time;
+		return diff > ONE_WEEK_IN_MS;
+	};
 
 	return (
 		<StyledContent>
@@ -42,6 +67,31 @@ export const ManageMainLayout = () => {
 
 			{remediations.length === 0 && globalRemediations.length === 0 && (
 				<EmptyManageMessage />
+			)}
+			{!IS_PRO_PLAN && isShowUpgradeAlert() && (
+				<Alert
+					icon={<CrownFilled color="promotion" />}
+					color="error"
+					onClose={hideUpgradeAlert}
+					sx={{ mt: 2 }}
+				>
+					<Typography variant="body2" sx={{ mb: 1.5, color: 'text.secondary' }}>
+						{__(
+							'Resolve these issues on all of your scanned pages with a click.',
+							'pojo-accessibility',
+						)}
+					</Typography>
+					<Button
+						size="small"
+						color="promotion"
+						variant="contained"
+						href={UPGRADE_URL}
+						target="_blank"
+						rel="noreferrer"
+					>
+						{__('Upgrade now', 'pojo-accessibility')}
+					</Button>
+				</Alert>
 			)}
 		</StyledContent>
 	);
