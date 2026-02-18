9fcffffd33d7c9b7ed545daf1317f8f153225c15
Nirbhay Singh <nirbhayr@elementor.com>
2026-02-18 22:08:41 +0530

    add: error handling

---
+++ classes/client/client-response.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace EA11y\Classes\Client;
+
+use Exception;
+use EA11y\Classes\Exceptions\Quota_Exceeded_Error;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class Client_Response {
+	private array $known_errors;
+	/**
+	 * @var mixed
+	 */
+	private $response;
+
+	/**
+	 * @throws Quota_Exceeded_Error|Exception
+	 */
+	public function handle() {
+		if ( ! is_wp_error( $this->response ) ) {
+			return $this->response;
+		}
+
+		$message = $this->response->get_error_message();
+
+		if ( isset( $this->known_errors[ $message ] ) ) {
+			throw $this->known_errors[ $message ];
+		}
+
+		throw new Exception( $message );
+	}
+
+	public function __construct( $response ) {
+		$this->known_errors = [
+			'Quota Status Guard Request Failed!: plan.features.ai_credits Quota exceeded' => new Quota_Exceeded_Error(),
+		];
+
+		$this->response = $response;
+	}
+}
