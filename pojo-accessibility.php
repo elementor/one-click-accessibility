964806da4d5a0aa8fb3aefb40dc395cd0eba58f6
Raz Ohad <admin@bainternet.info>
2026-03-31 19:03:44 +0700

    added license attribution

--- pojo-accessibility.php
+++ pojo-accessibility.php
@@ -7,6 +7,16 @@
  * Author URI: https://elementor.com/
  * Version: 4.1.1
  * Text Domain: pojo-accessibility
+ *
+ * Ally is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Ally is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
