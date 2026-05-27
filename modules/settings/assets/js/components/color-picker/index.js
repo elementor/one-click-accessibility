d69efd12f36c41e9347708c5b0fba3fbf8e120cd
Rami Yushuvaev <ramiy@elementor.com>
2026-05-27 08:51:20 +0300

    [APP-2900] Fix: Heading structure in Ally admin screens[APP-2849] 10th B-day campaign banner

--- modules/settings/assets/js/components/color-picker/index.js
+++ modules/settings/assets/js/components/color-picker/index.js
@@ -1,5 +1,6 @@
 import Box from '@elementor/ui/Box';
 import FormControl from '@elementor/ui/FormControl';
+import FormLabel from '@elementor/ui/FormLabel';
 import Grid from '@elementor/ui/Grid';
 import Typography from '@elementor/ui/Typography';
 import { styled } from '@elementor/ui/styles';
@@ -28,15 +29,16 @@ const ColorPicker = () => {
 
 	return (
 		<FormControl fullWidth>
-			<Typography
-				variant="subtitle2"
-				component="h3"
-				id="color-picker-label"
-				marginBottom={1}
-				color="text.primary"
-			>
-				{__('Color', 'pojo-accessibility')}
-			</Typography>
+			<FormLabel id="color-picker-label">
+				<Typography
+					variant="subtitle2"
+					component="div"
+					marginBottom={2}
+					color="text.secondary"
+				>
+					{__('Color', 'pojo-accessibility')}
+				</Typography>
+			</FormLabel>
 
 			<Grid padding={1} border={1} borderColor="divider" borderRadius={1}>
 				<HexColorPicker
--- modules/settings/assets/js/components/icon-radius/index.js
+++ modules/settings/assets/js/components/icon-radius/index.js
@@ -134,7 +134,7 @@ const IconRadius = () => {
 				color="secondary"
 				aria-label={__('Widget icon radius control', 'pojo-accessibility')}
 			>
-				<Typography variant="subtitle2" component="h3" marginBottom={1}>
+				<Typography variant="subtitle2" component="div" marginBottom={1}>
 					{__('Corner radius', 'pojo-accessibility')}
 				</Typography>
 			</FormLabel>
