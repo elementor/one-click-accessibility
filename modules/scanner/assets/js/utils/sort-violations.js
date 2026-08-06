e72da1795c7ebeb836fa753304fb33c76820a669
Nirbhay Singh <nirbhayr@elementor.com>
2026-08-06 15:41:17 +0530

    fix: add a fix for violations error

--- modules/scanner/assets/js/utils/sort-violations.js
+++ modules/scanner/assets/js/utils/sort-violations.js
@@ -7,17 +7,20 @@ export const sortViolations = (violations) => {
 	const sorted = structuredClone(INITIAL_SORTED_VIOLATIONS);
 
 	violations.forEach((item) => {
-		let type = '';
-		const outer = item.node.outerHTML;
-		const gtIndex = outer.indexOf('>');
-		item.snippet = gtIndex !== -1 ? outer.slice(0, gtIndex + 1) : item.snippet;
+		if (item?.node) {
+			let type = '';
+			const outer = item.node.outerHTML;
+			const gtIndex = outer.indexOf('>');
+			item.snippet =
+				gtIndex !== -1 ? outer.slice(0, gtIndex + 1) : item.snippet;
 
-		Object.keys(VIOLATION_TYPES).forEach((key) => {
-			if (VIOLATION_TYPES[key].includes(item.ruleId)) {
-				type = key;
-			}
-		});
-		sorted[type || 'other'].push(item);
+			Object.keys(VIOLATION_TYPES).forEach((key) => {
+				if (VIOLATION_TYPES[key].includes(item.ruleId)) {
+					type = key;
+				}
+			});
+			sorted[type || 'other'].push(item);
+		}
 	});
 
 	return sorted;
